687bb64913eac4bf91aa065213cb9de5cb085b81
cammac <camm@rocktechcomputer.com>
2019-03-01 08:53:07 -0800

    completed arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -88,11 +88,23 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i].first_name + ' ' + graduates[i].email);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let i = 0; i < graduates.length; i++) {
+  const result = [];
+  result.push(graduates[i].university);
+  for (let i = 0; i < result.length; i++) {
+    if (result[i].includes('Uni')) {
+      uni.push(result[i]);
+    }
+  }
+}
 console.log(uni);
 
 
